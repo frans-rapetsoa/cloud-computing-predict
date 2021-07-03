f6cc486bdb3e7b05ad8eef23734b76767470bdaa
frans-rapetsoa <86111671+frans-rapetsoa@users.noreply.github.com>
2021-07-03 22:27:20 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://dj0i3jzl3c.execute-api.eu-west-1.amazonaws.com/default/fransr-lamda-function",
+                url: "https://5ufvfmwot1.execute-api.eu-west-1.amazonaws.com/",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
