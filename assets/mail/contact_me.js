4c2ce4223c00eeff1c9e4fac493b741a325a57b9
frans-rapetsoa <86111671+frans-rapetsoa@users.noreply.github.com>
2021-07-03 17:58:52 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://o1oel0zad7.execute-api.us-east-2.amazonaws.com/",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
